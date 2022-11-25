25a8d7ed320eaab7999f80cb5c4270128ef3d9ea
john <john310897@gmail.com>
2022-11-25 22:48:17 +0530

    api detail fetching bug fixed

--- src/Services/cryptoApiDetails.js
+++ src/Services/cryptoApiDetails.js
@@ -1,18 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoAPIHeaders = {
-
+  'X-RapidAPI-Key': '3d16671d39mshefac45312ac2dd8p1b96b4jsnb4da2949a358',
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 };
-const createRequest = (urls) => ({ urls, headers: cryptoAPIHeaders });
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+
+const createRequest = (urls) => ({ url:urls, headers: cryptoAPIHeaders });
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/';
 export const cryptoApiDetails = createApi({
   reducerPath: 'cryptoApiDetails',
   baseQuery: fetchBaseQuery({ baseUrl: baseUrl }),
   endpoints: (builder) => ({
-
     getCryptoDetails: builder.query({
-      query: (coinId) => createRequest(`/coin/${coinId}`),
+      query: (coinId) => createRequest(`coin/${coinId}`),
     }),
   }),
 });
